c4ab724fad7ee6b639c94ec00784efbc8c58f3bd
roman-ier <ier@live.ru>
2020-09-28 22:37:51 +0300

    82 reselect

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -89,6 +89,7 @@ export const toggleFollowingProgress = (isFetching, userId) => ({
 export const getUsers = (currentPage, pageSize) => {
     return (dispatch) => {
         dispatch(toggleIsFetching(true));
+        dispatch(setCurrentPage(currentPage));
         requestAPI.getUsers(currentPage, pageSize).then(data => {
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
