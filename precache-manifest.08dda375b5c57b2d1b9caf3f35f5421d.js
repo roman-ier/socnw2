6427ca073a8ead0bc5a31d15a5a5e28da9d1a6b0
roman-ier <ier@live.ru>
2020-10-11 16:41:57 +0300

    Updates

---
+++ precache-manifest.08dda375b5c57b2d1b9caf3f35f5421d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e21020579222cb8003104106ac031",
+    "url": "/socnw2/index.html"
+  },
+  {
+    "revision": "703c3e7d1e68c085e2a9",
+    "url": "/socnw2/static/css/3.47ac3550.chunk.css"
+  },
+  {
+    "revision": "1f201ceb6a8271393d98",
+    "url": "/socnw2/static/css/4.91d73796.chunk.css"
+  },
+  {
+    "revision": "fff8422d8920f42345dc",
+    "url": "/socnw2/static/css/main.0ffa69b7.chunk.css"
+  },
+  {
+    "revision": "892383bc51dab78bf992",
+    "url": "/socnw2/static/js/2.075bd909.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socnw2/static/js/2.075bd909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703c3e7d1e68c085e2a9",
+    "url": "/socnw2/static/js/3.02503371.chunk.js"
+  },
+  {
+    "revision": "1f201ceb6a8271393d98",
+    "url": "/socnw2/static/js/4.824c474b.chunk.js"
+  },
+  {
+    "revision": "fff8422d8920f42345dc",
+    "url": "/socnw2/static/js/main.d99e3c70.chunk.js"
+  },
+  {
+    "revision": "aae15932e3f83e1fa224",
+    "url": "/socnw2/static/js/runtime-main.0c78c9c5.js"
+  },
+  {
+    "revision": "d5f339ef694f5ed68d797b62a0d8b056",
+    "url": "/socnw2/static/media/img1.d5f339ef.jpg"
+  }
+]);
