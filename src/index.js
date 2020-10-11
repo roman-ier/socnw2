9395836722dd79c442b74796c9f4f547ce9048b0
roman-ier <ier@live.ru>
2020-10-11 17:11:48 +0300

    95.3 - deploy

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import store from './redux/redux-store';
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App store={store}/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 serviceWorker.unregister();
